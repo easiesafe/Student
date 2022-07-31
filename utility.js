7f598a600a5ea9916072586114add1ee0c208e93
easiesafe <s.ptrp45.sth@gmail.com>
2022-07-31 20:02:59 +0700

    first commit

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MTc4MTQ4LCJleHAiOjE2NTkxODE3NDgsIm5iZiI6MTY1OTE3ODE0OCwianRpIjoiRGNmOHF4NDE0U2lQbUpDayIsInN1YiI6OTUsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.pOo10l9mYWnLnGN7lK4BxkrW4Vmw486FNOtOzCzp_zA"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjcyMTU1LCJleHAiOjE2NTkyNzU3NTUsIm5iZiI6MTY1OTI3MjE1NSwianRpIjoiM2l2UlhqSnJ0OEZrZG5sbiIsInN1YiI6MTIzLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.-OxInRRHlbd0mjYxTYwoaII_36v4JbJO9uPWn95SA20"; // put access token
 const student = {
   student_id: '63364659', // replace with your full name.
   name: 'Phattraphon Soitha',
@@ -18,7 +18,7 @@ exports.findStudentbyId = function (student_id, cb) {
             method: "GET",
             uri: EXTERNAL_API+student_id,
             headers: {
-                Authorization: `Bearer ${accessToken}`
+                Authorization: `Bearer  ${accessToken}`
             }
         }).then((response) => {
             console.log('Sent');

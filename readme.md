ab7fdf21dcffa1857e582f97d05a1073951627a1
easiesafe <s.ptrp45.sth@gmail.com>
2022-08-21 16:45:31 +0700

    first commit

--- readme.md
+++ readme.md
@@ -8,6 +8,9 @@
 - ## 💻 Skill: 
 <p align="left">
 <a href="#">
+    <img class="rounded-t-lg" src="image/Dinoo.png" alt="">
+    <a>
+<a href="#">
     <img src="svg/dev/frameworks/laravel.svg" alt="laravel" style="vertical-align:top; margin:6px 4px">
   </a>  
 
--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjcyMTU1LCJleHAiOjE2NTkyNzU3NTUsIm5iZiI6MTY1OTI3MjE1NSwianRpIjoiM2l2UlhqSnJ0OEZrZG5sbiIsInN1YiI6MTIzLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.-OxInRRHlbd0mjYxTYwoaII_36v4JbJO9uPWn95SA20"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5NzczNjQzLCJleHAiOjE2NTk3NzcyNDMsIm5iZiI6MTY1OTc3MzY0MywianRpIjoiSXg2R2R3OUpuOVU2MmpXViIsInN1YiI6MTM2LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.kEk5YcY4af7tT0C18XBnOKdyJF0bSf7Qeyf3S3GXAII"; // put access token
 const student = {
   student_id: '63364659', // replace with your full name.
   name: 'Phattraphon Soitha',

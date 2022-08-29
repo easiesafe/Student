b6f6f934008b9443fd247b00b8b87dd72edeb1ac
easiesafe <s.ptrp45.sth@gmail.com>
2022-08-29 15:34:07 +0700

    first commit

--- utility.js
+++ utility.js
@@ -2,12 +2,12 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5NzczNjQzLCJleHAiOjE2NTk3NzcyNDMsIm5iZiI6MTY1OTc3MzY0MywianRpIjoiSXg2R2R3OUpuOVU2MmpXViIsInN1YiI6MTM2LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.kEk5YcY4af7tT0C18XBnOKdyJF0bSf7Qeyf3S3GXAII"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzYxNTAyLCJleHAiOjE2NjE3NjUxMDIsIm5iZiI6MTY2MTc2MTUwMiwianRpIjoiZ1VadExzZHNxUXd5RUdGaiIsInN1YiI6MTQ3LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.1I96SF5ojPrA04jgwgQupVq13atlMMhctl6zYCAaXLg"; // put access token
 const student = {
   student_id: '63364659', // replace with your full name.
   name: 'Phattraphon Soitha',
   age: 20, // put your age.
-  gender: '', // replace with your gender
+  gender: 'male', // replace with your gender
   department: 'CPE'
 };
 
